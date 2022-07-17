3b8f34a280c58449080c3f94b3e78361f20b0875
samiullahbahadur <bahadursamiullah@gmail.com>
2022-07-17 13:17:59 +0430

    setup rails

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,24 @@
+class TodosController < ApplicationController
+  
+before_action :set_todo only: [:show, :update, :destroy]
+
+def create 
+    @todo=Todo.new(todo_params)
+    if @todo.save 
+      redirect_to todos_path
+    else
+      render 'new'
+    end
+  end
+
+  def new 
+    @todo=Todo.new
+ end
+  private
+  def set_todo
+    @todo=Todo.find(params[:id])
+  end
+  def todo_params
+    params.require(:todo).permit(:title, :completed)
+
+end
