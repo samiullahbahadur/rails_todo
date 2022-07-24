91329af4a1cf36563a11177b67fac4686e2b600a
samiullahbahadur <bahadursamiullah@gmail.com>
2022-07-24 12:04:27 +0430

    fix controller error

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,6 @@
 class TodosController < ApplicationController
   
-before_action :set_todo only: [:show, :update, :destroy]
+before_action :set_todo, only: [:show, :update, :destroy]
 
 def create 
     @todo=Todo.new(todo_params)
@@ -14,11 +14,12 @@ def create
   def new 
     @todo=Todo.new
  end
+
   private
   def set_todo
     @todo=Todo.find(params[:id])
   end
   def todo_params
     params.require(:todo).permit(:title, :completed)
-
+  end
 end
